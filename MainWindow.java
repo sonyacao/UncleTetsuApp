d64afd0eddb7c2e958851e9fcfb040e63ab0daab
sonyacao <34992109+sonyacao@users.noreply.github.com>
2018-01-08 20:28:15 -0500

    Add files via upload

--- MainWindow.java
+++ MainWindow.java
@@ -1,11 +1,18 @@
-package com.jrg.rca.main;
-
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.browser.Browser;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.CLabel;
+import org.eclipse.wb.swt.SWTResourceManager;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Label;
 
 public class MainWindow {
 
-	protected Shell shell;
+	protected Shell shlLogin;
+	private Text passwordField;
+	private Text usernameField;
 
 	/**
 	 * Launch the application.
@@ -26,9 +33,9 @@ public class MainWindow {
 	public void open() {
 		Display display = Display.getDefault();
 		createContents();
-		shell.open();
-		shell.layout();
-		while (!shell.isDisposed()) {
+		shlLogin.open();
+		shlLogin.layout();
+		while (!shlLogin.isDisposed()) {
 			if (!display.readAndDispatch()) {
 				display.sleep();
 			}
@@ -39,10 +46,42 @@ public class MainWindow {
 	 * Create contents of the window.
 	 */
 	protected void createContents() {
-		shell = new Shell();
-		shell.setSize(450, 300);
-		shell.setText("SWT Application");
+		shlLogin = new Shell();
+		shlLogin.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
+		shlLogin.setSize(450, 604);
+		shlLogin.setText("Login");
+		
+		CLabel uncleTetsuImage = new CLabel(shlLogin, SWT.NONE);
+		uncleTetsuImage.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
+		uncleTetsuImage.setRightMargin(6);
+		uncleTetsuImage.setLeftMargin(6);
+		uncleTetsuImage.setImage(SWTResourceManager.getImage(MainWindow.class, "/org/eclipse/wb/swt/aboutpic.png"));
+		uncleTetsuImage.setBounds(115, 22, 217, 335);
+		uncleTetsuImage.setText("");
+		
+		passwordField = new Text(shlLogin, SWT.BORDER);
+		passwordField.setBounds(182, 428, 110, 19);
+		
+		Button loginButton = new Button(shlLogin, SWT.NONE);
+		loginButton.setBounds(142, 474, 95, 28);
+		loginButton.setText("Login");
+		
+		usernameField = new Text(shlLogin, SWT.BORDER);
+		usernameField.setBounds(182, 386, 110, 19);
+		
+		Label usernameLabel = new Label(shlLogin, SWT.NONE);
+		usernameLabel.setForeground(SWTResourceManager.getColor(SWT.COLOR_RED));
+		usernameLabel.setBounds(86, 389, 60, 14);
+		usernameLabel.setText("Username");
+		
+		Label passwordLabel = new Label(shlLogin, SWT.NONE);
+		passwordLabel.setForeground(SWTResourceManager.getColor(SWT.COLOR_RED));
+		passwordLabel.setBounds(85, 431, 60, 14);
+		passwordLabel.setText("Password");
+		
+		Button newAccountButton = new Button(shlLogin, SWT.NONE);
+		newAccountButton.setBounds(86, 508, 217, 28);
+		newAccountButton.setText("Create New Account");
 
 	}
-
 }
